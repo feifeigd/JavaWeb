5d1a1618422ad8c667775933e40db5883569d7ec
luofei <13632323692@139.com>
2016-06-21 15:08:40 +0800

    add MessageConvertForSys.java

---
+++ internet-core/src/main/java/com/internet/cms/MessageConvertForSys.java
@@ -0,0 +1,30 @@
+package com.internet.cms;
+
+// activemq-core
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.ObjectMessage;
+import javax.jms.Session;
+
+import org.springframework.jms.support.converter.MessageConversionException;
+import org.springframework.jms.support.converter.MessageConverter;
+
+public class MessageConvertForSys implements MessageConverter{
+
+	public Object fromMessage(Message message) throws JMSException,
+			MessageConversionException {
+		System.out.println("[fromMessage]");
+		ObjectMessage objectMessage = (ObjectMessage)message;
+		return objectMessage.getObjectProperty("key1");
+	}
+
+	public Message toMessage(Object object, Session session) throws JMSException,
+			MessageConversionException {
+		System.out.println("[toMessage]");
+		ObjectMessage objectMessage = session.createObjectMessage();
+		objectMessage.setJMSExpiration(1000);
+		objectMessage.setStringProperty("key1", object + "_add");
+		return objectMessage;
+	}
+	
+}
