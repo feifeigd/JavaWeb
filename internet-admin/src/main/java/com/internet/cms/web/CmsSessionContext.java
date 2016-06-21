4754bffadb5d271a8476f152c8d1a1a3a95f25b7
luofei <13632323692@139.com>
2016-06-21 17:42:01 +0800

    add AdminController.java

---
+++ internet-admin/src/main/java/com/internet/cms/web/CmsSessionContext.java
@@ -0,0 +1,16 @@
+package com.internet.cms.web;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+public class CmsSessionContext {
+	private static final Map<String, HttpSession> ctx = new HashMap<String, HttpSession>();
+	public static void addSession(HttpSession session){
+		ctx.put(session.getId(), session);
+	}
+	public static void removeSession(HttpSession session) {
+		ctx.remove(session.getId());
+	}
+}
