b9a4bca8f5341b0a2047480cc471834f99524ab2
luofei <13632323692@139.com>
2016-06-23 10:38:57 +0800

    LoginController.java change package name

---
+++ internet-admin/src/main/java/com/internet/cms/dwr/DwrService.java
@@ -0,0 +1,43 @@
+package com.internet.cms.dwr;
+
+import org.directwebremoting.annotations.RemoteProxy;
+
+@RemoteProxy(name="dwrService")
+public class DwrService implements IDwrService{
+
+	public void addGroupChannel(int gid, int cid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void deleteGroupChannel(int gid, int cid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void updateIndexPic(int aid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void updateAttachInfo(int aid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void deleteAttach(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void updatePicPos(int id, int oldPos, int newPos) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void updateLinkPos(int id, int olePos, int newPos) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
