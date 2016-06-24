25abec2d51166a72f19ca0d15882d0c0d1685569
luofei <13632323692@139.com>
2016-06-24 17:26:04 +0800

    设置sring的Annotation扫描目录

---
+++ internet-core/src/main/java/com/internet/cms/service/user/RoleService.java
@@ -0,0 +1,18 @@
+package com.internet.cms.service.user;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.internet.cms.model.Role;
+import com.internet.cms.model.UserRole;
+
+@Service("roleService")
+public class RoleService implements IRoleService {
+
+	public List<Role> listRole(List<UserRole> urs) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ internet-core/src/main/java/com/internet/cms/service/user/UserService.java
@@ -0,0 +1,23 @@
+package com.internet.cms.service.user;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.internet.cms.model.User;
+import com.internet.cms.model.UserRole;
+
+@Service
+public class UserService implements IUserService {
+
+	public User login(String username, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<UserRole> listUserRoles(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
